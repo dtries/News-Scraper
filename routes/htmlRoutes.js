3505497f448362bd8689d6724c9f5e1bcaf7c83a
Dennis Ries <dtries@gmail.com>
2019-03-31 11:37:59 -0600

    basic html route now works, needs styling

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,13 @@
 const db = require("../models");
 
 module.exports = function (app) {
+    //Load index page
+    app.get("/", (req, res) => {
+        res.render("index"); // load the home page
+    });
+
+    app.get("*", (req, res) => {
+        res.render("404"); //load 404 on get error
+    })
 
 };
