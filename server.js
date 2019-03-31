560ee48122f0655160bb05ac127b3740218e0974
Dennis Ries <dtries@gmail.com>
2019-03-31 10:01:13 -0600

    GET article by ID now works

--- server.js
+++ server.js
@@ -26,7 +26,12 @@ app.use(express.static("public")); //Set index file in public folder as the home
 // Connect to the Mongo DB, use deployed database unless running on local server, then use local database
 // const MONGODB_URI = process.env.MONGODB_URI || "monogdb://localhost/mongoHeadlines";
 // mongoose.connect((MONGODB_URI), { useNewURLParser: true });
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+});
 
 // ======= ROUTES ========
 
@@ -72,7 +77,7 @@ app.get("/scrape", function (req, res) {
 });
 
 // Route to retrieve all articles from database
-app.get("/articles", function(req, res){
+app.get("/articles", (req, res) => {
     db.Article.find({}) // calls the article model to get all articles
         .then(dbArticle => {
             res.json(dbArticle);
@@ -80,6 +85,42 @@ app.get("/articles", function(req, res){
         .catch(err => {
             res.json(err);
         });
+});
+
+//Route to retrieve an article by id
+app.get("/articles/:id", (req, res) => {
+    let id = String(req.params.id);
+    // console.log(`The id for the article is ${id}`);
+    db.Article.findById(id)
+    .populate("note")
+    .then(dbArticle => {
+        res.json(dbArticle);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+});
+
+// Route to save or update note associated with article
+app.post("/articles/:id", (req, res) => {
+    let id = String(req.params.id);
+    db.Note.create(req.body) //create note for article using model
+        .then(dbNote => {
+            return db.Article.findById({
+                id
+            }, {
+                note: dbNote._id
+            }, {
+                new: true
+            });
+        })
+        .then(dbArticle => {
+            res.json(dbArticle);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 });
 
 app.listen(PORT, function () {
